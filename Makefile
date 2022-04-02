a48460a000f96985504114e25febd374149d86c9
Mathis Dankou <PetitEsprit@users.noreply.github.com>
2022-04-02 06:18:42 +0200

    humble beginnings

---
+++ Makefile
@@ -0,0 +1,48 @@
+_SRCS = main.c 
+_BONUS = 
+
+SRCS = $(patsubst %.c, src/%.c, $(_SRCS))
+OBJS = $(patsubst %.c, bin/%.o, $(_SRCS))
+
+BONUS = $(patsubst %.c, src/%.c, $(_BONUS))
+OBJS_BONUS = $(patsubst %.c, bin/%.o, $(_BONUS))
+
+INCS = -I./inc -I./libft -I./mlx_linux
+LIBS = -L./libft -lft -L./mlx_linux -lmlx_Linux -lXext -lX11 -lz -lm
+
+NAME = bin/fdf
+
+CC = cc
+CFLAGS = -Wall -Wextra -Werror
+
+all: $(NAME)
+
+libft/libft.a:
+	$(MAKE) bonus -C libft/ libft.a
+
+mlx_linux/libmlx_Linux.a:
+	$(MAKE) -C mlx_linux/ libmlx_Linux.a
+
+$(NAME): $(OBJS) libft/libft.a mlx_linux/libmlx_Linux.a
+	cc $(CFLAGS) $(INCS) $^ -o $@ $(LIBS)
+bonus: $(OBJS_BONUS) libft/libft.a mlx_linux/libmlx_Linux.a
+	cc $(CFLAGS) $(INCS) $^ -o $(NAME) $(LIBS)
+
+bin/%.o: src/%.c
+	mkdir -p bin
+	cc $(CFLAGS) $(INCS) -c $< -o $@
+
+check-norme:
+	norminette
+
+clean:
+	rm -f ${OBJS} ${OBJS_BONUS}
+	$(MAKE) fclean -C libft/
+	$(MAKE) clean -C mlx_linux/
+
+fclean: clean
+	rm -f $(NAME)
+
+re: fclean all
+
+.PHONY: all check-norme bonus clean fclean re
---
+++ src/main.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: user <user@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/02 05:41:57 by user              #+#    #+#             */
+/*   Updated: 2022/04/02 06:07:32 by user             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <mlx.h>
+
+typedef struct	s_vars {
+	void	*mlx;
+	void	*win;
+}				t_vars;
+
+int	close(int keycode, t_vars *vars)
+{
+	(void) keycode;
+	mlx_destroy_window(vars->mlx, vars->win);
+	return (0);
+}
+
+int	main(void)
+{
+	t_vars	vars;
+
+	vars.mlx = mlx_init();
+	vars.win = mlx_new_window(vars.mlx, 1920, 1080, "Hello world!");
+	mlx_hook(vars.win, 2, 1L<<0, close, &vars);
+	mlx_loop(vars.mlx);
+}
+
